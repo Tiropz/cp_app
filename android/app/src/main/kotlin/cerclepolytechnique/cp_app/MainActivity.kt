943e81ddda8b6aa8a8159c0a569dbe72eb604e84
Ilan Rossler <rosslerilan99@gmail.com>
2020-07-09 12:43:59 +0200

    init

---
+++ android/app/src/main/kotlin/cerclepolytechnique/cp_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package cerclepolytechnique.cp_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,111 @@
+import 'package:cp_app/components/bottom_navy_bar.dart';
+import 'package:cp_app/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:cp_app/services/authentication.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:cp_app/models/todo.dart';
+import 'dart:async';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.auth, this.userId, this.logoutCallback})
+      : super(key: key);
+
+  final BaseAuth auth;
+  final VoidCallback logoutCallback;
+  final String userId;
+
+  @override
+  State<StatefulWidget> createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  //bool _isEmailVerified = false;
+
+  int _currentIndex = 0;
+  PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  signOut() async {
+    try {
+      await widget.auth.signOut();
+      widget.logoutCallback();
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[800],
+      body: SizedBox.expand(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() => _currentIndex = index);
+          },
+          children: <Widget>[
+            Container(color: Color.fromRGBO(58, 66, 86, 1)),
+            Container(color: Colors.grey[800],),
+            Container(color: Colors.grey[800],),
+            Container(color: Colors.grey[800],),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 0, 0, 5),
+        child: BottomNavyBar(
+
+          backgroundColor: Color.fromRGBO(58, 66, 86, 1),
+          selectedIndex: _currentIndex,
+          onItemSelected: (index) {
+            setState(() => _currentIndex = index);
+            _pageController.jumpToPage(index);
+          },
+          items: <BottomNavyBarItem>[
+            BottomNavyBarItem(
+                textAlign: TextAlign.center,
+                activeColor: Colors.teal,
+                title: Text('News'),
+                icon: Icon(Icons.library_books)
+            ),
+            BottomNavyBarItem(
+                textAlign: TextAlign.center,
+                activeColor: Colors.teal,
+                title: Text('Calendrier'),
+                icon: Icon(Icons.calendar_today)
+            ),
+            BottomNavyBarItem(
+                textAlign: TextAlign.center,
+                activeColor: Colors.teal,
+                title: Text('Contact'),
+                icon: Icon(Icons.info)
+            ),
+            BottomNavyBarItem(
+                textAlign: TextAlign.center,
+                activeColor: Colors.teal,
+                title: Text('Paramètres'),
+                icon: Icon(Icons.settings)
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
